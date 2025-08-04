1c43e165d1f1575eaa64971e5143326f9d9561c7
sekiakari <akr.60805@gmail.com>
2025-08-04 23:03:11 +0900

    Login機能の実装

---
+++ src/resources/views/auth/login.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/auth/login.css')}}">
+@endsection
+
+@section('content')
+<div class="login-form">
+    <h2 class="login-form-heading">ログイン</h2>
+    <div class="login-form-inner">
+        <form class="login-form-form" action="{{ route('login') }}" method="post">
+        @csrf
+            <div class="login-form-group">
+                <label class="login-form-label" for="email">メールアドレス</label>
+                <input class="login-form-input" type="mail" name="email" id="email">
+                <p class="login-form-error-message">
+                @error('email')
+                {{ $message }}
+                @enderror
+                </p>
+            </div>
+            <div class="login-form-group">
+                <label class="login-form-label" for="password">パスワード</label>
+                <input class="login-form-input" type="password" name="password" id="password">
+                <p class="login-form-error-message">
+                @error('password')
+                {{ $message }}
+                @enderror
+                </p>
+            </div>
+            <input class="login-form-btn" type="submit" value="ログインする">
+            <a href="{{ route('register') }}" class="register-link">会員登録はこちら</a>
+        </form>
+    </div>
+</div>
+@endsection
---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COACHTECH</title>
+    <link rel="stylesheet" href="{{ asset('css/common.css')}}">
+    @yield('css')
+</head>
+<body>
+    <div class="app">
+        <header class="header">
+            <img src="{{ asset('images/logo.svg') }}" alt="ロゴ">
+            @yield('link')
+        </header>
+        <div class="content">
+            @yield('content')
+        </div>
+    </div>
+</body>
+</html>
