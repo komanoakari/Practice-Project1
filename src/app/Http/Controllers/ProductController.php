59988a27a1844982c2d2545b5f284d7984186481
sekiakari <akr.60805@gmail.com>
2025-09-03 21:58:52 +0900

    WIP: 作業中の変更を保存

--- src/app/Http/Controllers/ProductController.php
+++ src/app/Http/Controllers/ProductController.php
@@ -1,22 +1,83 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\Product;
-use App\Models\Profile;
 
+use Illuminate\Support\Facades\DB;
+use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Requests\ExhibitionRequest;
+use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function getProducts(Request $request)
     {
-        $products = Product::all();
-        return view('list', compact('products'));
+        $uid = Auth::id();
+        $tab = $request->query('tab', 'recommended');
+
+        if ($tab === 'mylist') {
+            if (!$uid) {
+                $products = collect();
+            } else {
+                $products = Auth::user()
+                ->mylistProducts()
+                ->latest()
+                ->get();
+            }
+        } else {
+            $query = Product::query();
+            if ($uid) {
+                $query->where(function ($q) use ($uid) {
+                    $q->whereNull('user_id')->orWhere('user_id', '!=', $uid);
+                });
+            }
+            $products = $query->latest()->get();
+        }
+        return view('list', compact('products', 'tab'));
     }
 
-    public function show()
+    public function getDetail(Product $product)
     {
-        $profiles = Profile::all();
-        return view('edit', compact('profiles'));
+        $product->load('categories')
+                ->loadCount(['comments', 'wishlistBy as likes_count']);
+
+        $liked = auth()->check()
+            ? $product->wishlistBy()->where('user_id', auth()->id())->exists()
+            : false;
+
+        return view('detail', compact('product', 'liked'));
+    }
+
+    public function create()
+    {
+        $categories = Category::orderBy('id')->get();
+        return view('listing', compact('categories'));
+    }
+
+    public function storeListing(ExhibitionRequest $request)
+    {
+        $dir = "images";
+
+        $file_name = $request->file('image')->getClientOriginalName();
+        $request->file('image')->storeAs('public/'.$dir, $file_name);
+        $imagePathView = 'storage/'.$dir.'/'.$file_name;
+
+        DB::transaction(function () use ($request, $imagePathView){
+            $product = new Product();
+            $product->name = $request->input('name');
+            $product->brand = $request->input('brand');
+            $product->price = $request->input('price');
+            $product->image = $imagePathView;
+            $product->description = $request->input('description');
+            $product->condition = $request->input('condition');
+            $product->save();
+
+            $categoryIds = $request->input('category', []);
+
+            $product->categories()->sync($categoryIds);
+        });
+
+        return redirect('/')->with('status', '出品が完了しました');
     }
 }
