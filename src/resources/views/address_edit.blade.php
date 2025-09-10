b0f34fa69691dae699f2e631fcf0da35c13e4bed
sekiakari <akr.60805@gmail.com>
2025-09-10 20:22:52 +0900

    購入画面の実装

---
+++ src/resources/views/address_edit.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/address_edit.css') }}">
+@endsection
+
+@section('content')
+<div class="address-edit-contents">
+    <h2 class="address-edit-heading">住所の変更</h2>
+    <div class="address-edit-content">
+        <form action="{{ route('address.update', ['product' => $product->id]) }}" class="address-edit-form" method="post">
+            @csrf
+            @method('PUT')
+            <div class="address-edit-group">
+                <label for="shipping_postal_code" class="address-edit-label">郵便番号</label>
+                <input type="text" name="shipping_postal_code" id="shipping_postal_code" class="address-edit-input">
+                <p class="address-edit-error-message">
+                    @error('shipping_postal_code')
+                    {{ $message }}
+                    @enderror
+                </p>
+            </div>
+            <div class="address-edit-group">
+                <label for="shipping_address" class="address-edit-label">住所</label>
+                <input type="text" name="shipping_address" id="shipping_address" class="address-edit-input">
+                <p class="address-edit-error-message">
+                    @error('shipping_address')
+                    {{ $message }}
+                    @enderror
+                </p>
+            </div>
+            <div class="address-edit-group">
+                <label for="shipping_building" class="address-edit-label">建物名</label>
+                <input type="text" name="shipping_building" id="shipping_building" class="address-edit-input">
+                <p class="address-edit-error-message">
+                    @error('shipping_building')
+                    {{ $message }}
+                    @enderror
+                </p>
+            </div>
+            <input type="submit" class="address-edit-btn" value="更新する">
+        </form>
+    </div>
+</div>
+@endsection
