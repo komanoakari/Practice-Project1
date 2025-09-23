a4200b82be2e37d1ce446cbfefe960b174faf029
sekiakari <akr.60805@gmail.com>
2025-09-23 14:44:06 +0900

    商品購入画面の実装

--- src/resources/views/profile/mypage.blade.php
+++ src/resources/views/profile/mypage.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
 @if (session('status'))
-    <div class="alert-success" style="color:#ff0000; font-size:24px;">
+    <div class="alert-success" style="font-size:24px;">
         {{ session('status') }}
     </div>
 @endif
