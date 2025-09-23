393fa7eeba532d93b98828f0c901c1d8aaec795f
sekiakari <akr.60805@gmail.com>
2025-09-23 14:44:06 +0900

    商品購入画面の実装

--- src/app/Http/Requests/AddressRequest.php
+++ src/app/Http/Requests/AddressRequest.php
@@ -6,12 +6,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class AddressRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-
     public function authorize()
     {
         return true;
